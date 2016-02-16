7cc1ca86ca21deff4fb9c4d042afa282b1987570
Marcus Peterson <mdabomb5@gmail.com>
2016-02-16 16:38:25 -0700

    added name field

--- main/models.py
+++ main/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Listing(models.Model):
+	name = models.CharField(max_length=70)
+
 	MALE = 'M'
 	FEMALE = 'F'
 	OTHER = 'Other'
