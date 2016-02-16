345e68de01ae07eb3cb2b96efc147194e5ea08a6
Marcus Peterson <mdabomb5@gmail.com>
2016-02-16 16:18:34 -0700

    models added

--- main/models.py
+++ main/models.py
@@ -2,4 +2,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Listing(models.Model):
+	MALE = 'M'
+	FEMALE = 'F'
+	OTHER = 'Other'
+	GENDER_CHOICES = (
+		(MALE, 'Male'),
+		(FEMALE, 'Female'),
+		(OTHER, 'Other'),
+		)
+	gender_choices = models.CharField(max_length=5, choices=GENDER_CHOICES, default=MALE)
+
+	SHARED = 'Shared'
+	PRIVATE = 'Private'
+	MARRIED = 'Married'
+	ROOM_TYPE = (
+		(SHARED, 'Shared'),
+		(PRIVATE, 'Private'),
+		(MARRIED, 'Married'),
+		)
+	room_type = models.CharField(max_length=7, choices=ROOM_TYPE, default=PRIVATE)
+
