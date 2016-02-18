a4b67681cee3d5fffc5c3a2f5d8099a2b800dd65
Ammon Heinzen <arheinzen@gmail.com>
2016-02-18 14:59:51 -0700

    BYU housing boolean

--- main/models.py
+++ main/models.py
@@ -23,32 +23,34 @@ class Listing(models.Model):
     )
     room_type = models.CharField(max_length=7, choices=ROOM_TYPE, default=PRIVATE)
 
-	FALL = 'Fall'
-	SUMMER = 'Summer'
-	WINTER = 'Winter'
-	FALL_WINTER = 'Fall/Winter'
-	WINTER_SUMMER = 'Winter/Summer'
-	SUMMER_FALL = 'Summer/Fall'
-	FULL_YEAR = 'Full Year'
-	OTHER = 'Other'
-	CONTRACT_LENGTH = (
-		(FALL, 'Fall'),
-		(SUMMER, 'Summer'),
-		(WINTER, 'Winter'),
-		(FALL_WINTER, 'Fall/Winter'),
-		(WINTER_SUMMER, 'Winter/Summer'),
-		(SUMMER_FALL, 'Summer/Fall'),
-		(FULL_YEAR, 'Full Year'),
-		(OTHER, 'Other'),
-		)
-	contract_length = models.CharField(max_length=15, choices=CONTRACT_LENGTH, default=FALL_WINTER)
-
-	APARTMENT = 'Apt'
-	HOUSE = 'House'
-	BUILDING_TYPE = (
-		(APARTMENT, 'Apartment'),
-		(HOUSE, 'House'),
-		)
-	building_type = models.CharField(max_length=10, choices=BUILDING_TYPE, default=APARTMENT)
+    FALL = 'Fall'
+    SUMMER = 'Summer'
+    WINTER = 'Winter'
+    FALL_WINTER = 'Fall/Winter'
+    WINTER_SUMMER = 'Winter/Summer'
+    SUMMER_FALL = 'Summer/Fall'
+    FULL_YEAR = 'Full Year'
+    OTHER = 'Other'
+    CONTRACT_LENGTH = (
+        (FALL, 'Fall'),
+        (SUMMER, 'Summer'),
+        (WINTER, 'Winter'),
+        (FALL_WINTER, 'Fall/Winter'),
+        (WINTER_SUMMER, 'Winter/Summer'),
+        (SUMMER_FALL, 'Summer/Fall'),
+        (FULL_YEAR, 'Full Year'),
+        (OTHER, 'Other'),
+    )
+    contract_length = models.CharField(max_length=15, choices=CONTRACT_LENGTH, default=FALL_WINTER)
+    
+    APARTMENT = 'Apt'
+    HOUSE = 'House'
+    BUILDING_TYPE = (
+        (APARTMENT, 'Apartment'),
+        (HOUSE, 'House'),
+    )
+    building_type = models.CharField(max_length=10, choices=BUILDING_TYPE, default=APARTMENT)
+    
+    BYU_housing = models.BooleanField(default=True)
 
 
