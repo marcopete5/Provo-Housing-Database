82b44d4554da1db7efff1e93b8a84d59edcf21e4
Dan Hitt <d.hitt424@gmail.com>
2016-02-18 15:16:29 -0700

    fixed furnishing

--- main/models.py
+++ main/models.py
@@ -29,15 +29,15 @@ class Listing(models.Model):
         )
     room_type = models.CharField(max_length=7, choices=ROOM_TYPE, default=PRIVATE)
 
-    FURNISHED = 'Furnished' #dan
+    FURNISHED = 'Furnished' 
     PARTIAL_FURNISHED = 'Partial Furnished' 
     UNFURNISHED = 'Unfurnished' 
     FURNISHINGS = (
-        (FURNISHED,'Furnished')
-        (PARTIAL_FURNISHED , 'Partial Furnished')
-        (UNFURNISHED, 'Unfurnished')
+        (FURNISHED,'Furnished'),
+        (PARTIAL_FURNISHED, 'Partial Furnished'),
+        (UNFURNISHED, 'Unfurnished'),
         ) 
-    furnishings_type = models.CharField(max_length=20, choices=FURNISHINGS, default=UNFURNISHED) #dan
+    furnishings_type = models.CharField(max_length=20, choices=FURNISHINGS, default=UNFURNISHED) 
 
     FALL = 'Fall'
     SUMMER = 'Summer'
